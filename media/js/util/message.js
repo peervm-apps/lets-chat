706a02f160c5da9539afbf28cf099afe8ac32b9b
Simon Bartlett <simon@securitycompass.com>
2015-03-17 13:36:05 -0400

    Added support for a subset of markdown

--- media/js/util/message.js
+++ media/js/util/message.js
@@ -15,6 +15,37 @@ if (typeof exports !== 'undefined') {
     // Message Text Formatting
     //
 
+    var markdownFormatter = window.markdownit('zero', {
+        html: false
+    }).enable([ 'backticks', 'emphasis', 'link', 'linkify' ]);
+
+    var textFormatter = window.markdownit('zero', {
+        html: false,
+        breaks: false,
+        linkify: true
+    });
+
+    function markdownFormat(text) {
+        return markdownFormatter
+            .render(text)
+            .replace(/<a /g, '<a target="_blank" ');
+    }
+
+    function textFormat(text) {
+        var paste = /\n/i.test(text);
+
+        var formatted = textFormatter
+            .render(text)
+            .replace(/<a /g, '<a target="_blank" ');
+
+
+        if (paste && formatted.length) {
+            return '<pre>' +
+                formatted.substring(3, formatted.length - 7) + '</pre>';
+        }
+
+        return formatted;
+    }
 
     function getBaseUrl() {
         var parts = window.location.pathname.split('/');
@@ -74,23 +105,21 @@ if (typeof exports !== 'undefined') {
         });
     }
 
-    function links(text) {
-        var imagePattern = /^\s*((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|][.](jpe?g|png|gif))\s*$/i,
-        linkPattern = /((https?|ftp):\/\/[-A-Z0-9+&*@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    function embeds(text) {
+        text = uploads(text);
 
-        if (imagePattern.test(text)) {
-            return text.replace(imagePattern, function(url) {
-                var uri = encodeURI(_.unescape(url));
-                return '<a class="thumbnail" href="' + uri +
-                       '" target="_blank"><img src="' + uri +
-                       '" alt="Pasted Image" /></a>';
-            });
-        } else {
-            return text.replace(linkPattern, function(url) {
-                var uri = encodeURI(_.unescape(url));
-                return '<a href="' + uri + '" target="_blank">' + url + '</a>';
-            });
+        var imagePattern = /^\s*((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|][.](jpe?g|png|gif))\s*$/i;
+
+        if (!text.match(imagePattern)) {
+            return false;
         }
+
+        return text.replace(imagePattern, function(url) {
+            var uri = encodeURI(_.unescape(url));
+            return '<a class="thumbnail" href="' + uri +
+                   '" target="_blank"><img src="' + uri +
+                   '" alt="Pasted Image" /></a>';
+        });
     }
 
     function emotes(text, data) {
@@ -122,12 +151,17 @@ if (typeof exports !== 'undefined') {
     }
 
     exports.format = function(text, data) {
+        var embed = embeds(text);
+        if (embed) {
+            return embed;
+        }
+
+        var pasted = /\n/i.test(text);
+
         var pipeline = [
-            trim,
+            pasted ? textFormat : markdownFormat,
             mentions,
             roomLinks,
-            uploads,
-            links,
             emotes,
             replacements
         ];
